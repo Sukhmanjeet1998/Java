aea92a23c54669fbcaac479677bf4f2414b193d9
Sukhmanjeet Singh <149586206+Sukhmanjeet1998@users.noreply.github.com>
2024-10-11 15:55:43 +0000

    JavaBasic

---
+++ JavaBasic1.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+public class JavaBasic1 {
+    public static void main(String args[]) {
+        // Output
+        /*
+         * System.out.print("Hello World\n");
+         * System.out.println("Lets Start Java");
+         * 
+         * int a = 5;
+         * int b = 6;
+         * System.out.println(a+b);
+         */
+
+        // Input
+        /*
+         * Scanner sc = new Scanner(System.in);
+         * System.out.println("Enter 1st number!");
+         * int num1 = sc.nextInt();
+         * System.out.println("Enter 2nd number!");
+         * int num2 = sc.nextInt();
+         * System.out.println("Enter radius of a circle!");
+         * int radius = sc.nextInt();
+         * float area = 3.14f * radius * radius;
+         * System.out.println("Sum of " + num1 + " and " + num2 + " is " + (num1+num2));
+         * System.out.println("Product of " + num1 + " and " + num2 + " is " +
+         * (num1*num2));
+         * System.out.println("Area of a circle with radius " + radius + " is " +
+         * (area));
+         */
+
+        // Type Promoting
+        /*
+        short s = 1;
+        byte b = 2;
+        char c = 'a';
+        System.out.println(s + b + c);
+         */
+
+    }
+}
---
+++ Problems.java
@@ -0,0 +1,108 @@
+import java.util.*;
+import java.lang.*;
+
+// public class Problems {
+
+// public static void main(String[] args) {
+
+// }
+
+// }
+
+/*
+ * Question1:In a program,input 3 numbers: A, B and C.You have to out put the
+ * average of these 3 numbers.(Hint : Average of N numbers is sum of those
+ * numbers divided by N)
+ */
+
+// public class Problems {
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// System.out.println("Enter 1st number");
+// int num1 = sc.nextInt();
+// System.out.println("Enter 2nd number");
+// int num2 = sc.nextInt();
+// System.out.println("Enter 3rd number");
+// int num3 = sc.nextInt();
+// sc.close();
+// float average = ((num1+num2+num3)/3f);
+// System.out.println("Average of "+ num1 + " and " + num2 + " and " + num3 + "
+// is " + average);
+// }
+
+// }
+
+// ................................................................................
+
+/*
+ * Question2:In a program,input the side of a square.You have to out put the
+ * area of the square.(Hint : area of a square is (side x side))
+ */
+
+// public class Problems {
+
+// public static void main(String[] args) {
+// Scanner sc = new Scanner(System.in);
+// System.out.println("Enter side of a square");
+// int side = sc.nextInt();
+// sc.close();
+// int area = side * side;
+// System.out.println("Area of a square with side " + side + "cm is " + area +
+// "cm^2");
+// }
+
+// }
+
+
+// ................................................................................
+
+/*
+ * Question3:Enter cost of 3 items from the user (using float datatype)-a
+ * pencil,a pen and an eraser. You have to output the total cost of the items
+ * back to the user as their bill.(Add on : You can also try adding 18% gst tax
+ * to the items in the bill as an advanced problem)
+ */
+
+// { Pending to do }
+
+
+// ................................................................................
+
+/*
+ * Question4: What will be the type of result in thefollowing Java code?
+ * byte b = 4;
+ * char c = 'a';
+ * short s = 512;
+ * int i = 1000;
+ * float f = 3.14f;
+ * double d = 99.9954;
+ * result = (f*b) + (i%c) - (d*s);
+ */
+
+// Answer:  The result variable will be of double type because oftype conversion.
+
+
+// ................................................................................
+
+/*
+ * Question 5:(Advanced)Will the following statement give any error in
+ * Java ? int $ = 24;
+ */
+
+// public class Problems {
+
+//     public static void main(String[] args) {
+//         int $my = 5;
+//         System.out.println($my);
+//     }
+
+// }
+
+// No, the statement will not give any
+// error.NamesofvariablesarecalledidentifiersinJava.Identifierrulesays,identifierscanstartwithany
+// alphabet or underscore (“_”) or dollar (“$”).According to the rule the given
+// variable name is a valid identifier.
+
+
+// ................................................................................
