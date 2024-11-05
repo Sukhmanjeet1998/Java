9b496b864d3a28439b7419d7f205fd2909694ae6
Sukhmanjeet Singh <149586206+Sukhmanjeet1998@users.noreply.github.com>
2024-11-05 14:58:14 +0000

    class and object

---
+++ JavaBasic9.java
@@ -0,0 +1,22 @@
+
+// Class and Object
+import java.util.*;
+import java.lang.*;
+
+class Calculator {
+    public int add(int num1, int num2) {
+        int result = num1 + num2;
+        return result;
+    }
+}
+
+public class JavaBasic9 {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        int n1 = 3;
+        int n2 = 3;
+        int result = calc.add(n2, n1);
+        System.out.println(result);
+    }
+
+}
